c627bd1e446bae0c44830861d3b2415750148c10
hldgaoshuo <hldgaoshuo@163.com>
2026-01-05 15:35:13 +0800

    feat(row): 添加对多种文本类型的支持并修改行分隔符
    - 添加了对 VARCHAR、TINY_TEXT、TEXT、MEDIUM_TEXT 和 LONG_TEXT 类型的支持
    - 将行分隔符从换行符(\n)更改为 NUL 字符(\0)
    - 更新了 new_row_from_bytes 函数以处理新的数据类型和分隔符
    - 修改了相关的测试用例以匹配新的分隔符和数据类型处理

--- row.py
+++ row.py
@@ -3,10 +3,15 @@ from io import BytesIO
 from value.big_int import new_big_int_from_bytes
 from value.char import new_char_from_bytes
 from value.int import new_int_from_bytes
+from value.long_text import new_long_text_from_bytes
 from value.medium_int import new_medium_int_from_bytes
+from value.medium_text import new_medium_text_from_bytes
 from value.small_int import new_small_int_from_bytes
+from value.text import new_text_from_bytes
 from value.tiny_int import new_tiny_int_from_bytes
+from value.tiny_text import new_tiny_text_from_bytes
 from value.value import Value, ValueType
+from value.varchar import new_varchar_from_bytes
 
 
 class Row:
@@ -19,7 +24,7 @@ class Row:
         for v in self.values:
             r += bytes(v)
         # 添加分隔符表示行结束
-        r += b'\n'  # 使用换行符作为行分隔符
+        r += b'\0'  # 使用 NUL 作为行分隔符
         return r
 
     def __eq__(self, other: 'Row') -> bool:
@@ -62,7 +67,22 @@ def new_row_from_bytes(buf: BytesIO) -> Row:
         elif value_type == ValueType.CHAR:
             value = new_char_from_bytes(buf)
             row.values_add(value)
-        elif value_type_bs == b'\n':
+        elif value_type == ValueType.VARCHAR:
+            value = new_varchar_from_bytes(buf)
+            row.values_add(value)
+        elif value_type == ValueType.TINY_TEXT:
+            value = new_tiny_text_from_bytes(buf)
+            row.values_add(value)
+        elif value_type == ValueType.TEXT:
+            value = new_text_from_bytes(buf)
+            row.values_add(value)
+        elif value_type == ValueType.MEDIUM_TEXT:
+            value = new_medium_text_from_bytes(buf)
+            row.values_add(value)
+        elif value_type == ValueType.LONG_TEXT:
+            value = new_long_text_from_bytes(buf)
+            row.values_add(value)
+        elif value_type_bs == b'\0':
             return row
         else:
             raise ValueError("未知数据类型")
--- row_test.py
+++ row_test.py
@@ -6,16 +6,16 @@ from value.char import new_char
 from value.int import new_int
 
 
-def test_bytes():
-    value1 = new_int(4000000000)
-    value2 = new_char("abc", 4)
-    row = new_row([value1, value2])
-    row_bs = bytes(row)
-    assert row_bs == b'\x04' + b'\xEE\x6B\x28\x00' + b'\x06' + b'\x04' + b'abc ' + b'\n'
+def test_new_row_from_bytes_int():
+    row_bs = b'\x04' + b'\xEE\x6B\x28\x00' + b'\0'
+    row = new_row_from_bytes(BytesIO(row_bs))
+    value = new_int(4000000000)
+    row_other = new_row([value])
+    assert row == row_other
 
 
-def test_new_row_from_bytes():
-    row_bs = b'\x04' + b'\xEE\x6B\x28\x00' + b'\x06' + b'\x04' + b'abc ' + b'\n'
+def test_new_row_from_bytes_int_char():
+    row_bs = b'\x04' + b'\xEE\x6B\x28\x00' + b'\x06' + b'\x04' + b'abc ' + b'\0'
     row = new_row_from_bytes(BytesIO(row_bs))
     value1 = new_int(4000000000)
     value2 = new_char("abc", 4)
